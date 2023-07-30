3f7ba5b17603e0ade12e761db282a8c6e75586fe
Romanov Alexander <alexander.romanov@syntacore.com>
2023-07-30 17:55:40 +0300

    options

--- src/main.cc
+++ src/main.cc
@@ -16,9 +16,26 @@ using namespace llvm;
 cl::OptionCategory Options("domfront options");
 static cl::opt<unsigned long long> Seed("seed", cl::desc("Random seed"),
                                         cl::cat(Options), cl::init(0));
-
+static cl::opt<unsigned long long>
+    GenIterations("gen-iterations",
+                  cl::desc("Iteration number for CFG generation"),
+                  cl::cat(Options), cl::init(10));
 static cl::opt<bool> DumpCFG("dump-cfg", cl::desc("Dump CFG"), cl::cat(Options),
                              cl::init(false));
+static cl::opt<bool> PrintDominators("print-dominators",
+                                     cl::desc("Print Dominators"),
+                                     cl::cat(Options), cl::init(false));
+
+static void Print(const NodetoDominatorsTy &Dominators) {
+  for (auto &&[NodePtr, Doms] : Dominators) {
+    std::cout << NodePtr->Val << ": ";
+    if (Doms) {
+      for (auto *Dom : Doms.value())
+        std::cout << Dom->Val << ", ";
+      std::cout << std::endl;
+    }
+  }
+}
 } // namespace lqvm
 
 using namespace lqvm;
@@ -28,13 +45,6 @@ int main(int Argc, char **Argv) {
   auto G = GB.generate();
   if (DumpCFG)
     G.dumpDot(std::cout);
-  auto Dominators = ComputeDominators(G);
-  for (auto &&[NodePtr, Doms] : Dominators) {
-    std::cerr << NodePtr->Val << ": ";
-    if (Doms) {
-      for (auto *Dom : Doms.value())
-        std::cerr << Dom->Val << ", ";
-      std::cerr << std::endl;
-    }
-  }
+  if (PrintDominators)
+    Print(ComputeDominators(G));
 }
