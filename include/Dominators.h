80c0e0e06294f0487191b4284792f490b33bc0ce
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-09-02 13:28:09 +0300

    re-initial commit

--- include/Dominators.h
+++ include/Dominators.h
@@ -10,6 +10,10 @@
 #include "Utils.h"
 
 #include <llvm/ADT/DenseMap.h>
+<<<<<<< HEAD
+=======
+#include <llvm/ADT/PostOrderIterator.h>
+    >>>>>>> 32e402f (re-initial commit)
 #include <llvm/ADT/STLExtras.h>
 #include <llvm/ADT/SetOperations.h>
 #include <llvm/ADT/SetVector.h>
@@ -19,55 +23,97 @@
 #include <optional>
 #include <unordered_map>
 #include <vector>
-namespace lqvm {
+    namespace lqvm {
 
-using NodetoDominatorsTy =
-    std::unordered_map<const Node *, std::vector<const Node *>>;
-inline size_t ComputeHash(const NodetoDominatorsTy &M) {
-  size_t Hash = 0;
-  for (auto &&[NodePtr, Doms] : M) {
-    for (auto *Dom : Doms)
-      Hash += std::hash<const Node *>()(Dom);
+  using NodetoDominatorsTy =
+      std::unordered_map<const Node *, std::vector<const Node *>>;
+  inline size_t ComputeHash(const NodetoDominatorsTy &M) {
+    size_t Hash = 0;
+    for (auto &&[NodePtr, Doms] : M) {
+      for (auto *Dom : Doms)
+        Hash += std::hash<const Node *>()(Dom);
+    }
+    return Hash;
   }
-  return Hash;
-}
 
-inline void ComputeDominatorsIteration(const GraphTy &G,
-                                       NodetoDominatorsTy &M) {
-  std::vector<const Node *> AllNodesSet;
-  for (auto &Nd : G)
-    AllNodesSet.push_back(&Nd);
-  for (auto &&Nd : G) {
-    std::vector<const Node *> Doms = AllNodesSet;
-    for (auto *Parent : Nd.Parents)
-      if (M.contains(Parent))
-        Doms = OrderedIntersection(Doms, M[Parent]);
-    Doms.push_back(&Nd);
-    if (!Nd.Parents.empty())
-      M[&Nd] = std::move(Doms);
+  inline void ComputeDominatorsIteration(const GraphTy &G,
+                                         NodetoDominatorsTy &M) {
+    std::vector<const Node *> AllNodesSet;
+    for (auto &Nd : G)
+      AllNodesSet.push_back(&Nd);
+    for (auto &&Nd : G) {
+      std::vector<const Node *> Doms = AllNodesSet;
+      for (auto *Parent : Nd.Parents)
+        if (M.contains(Parent))
+          Doms = OrderedIntersection(Doms, M[Parent]);
+      Doms.push_back(&Nd);
+      if (!Nd.Parents.empty())
+        M[&Nd] = std::move(Doms);
+    }
   }
-}
 
-inline NodetoDominatorsTy ComputeDominators(const GraphTy &G) {
-  NodetoDominatorsTy NodeToDominators;
-  NodeToDominators[&G.front()] = std::vector<const Node *>{&G.front()};
-  auto NewHash = ComputeHash(NodeToDominators);
-  auto OldHash = NewHash;
-  do {
-    OldHash = NewHash;
-    ComputeDominatorsIteration(G, NodeToDominators);
-    NewHash = ComputeHash(NodeToDominators);
-  } while (NewHash != OldHash);
-  return NodeToDominators;
-}
+  inline NodetoDominatorsTy ComputeDominators(const GraphTy &G) {
+    NodetoDominatorsTy NodeToDominators;
+    NodeToDominators[&G.front()] = std::vector<const Node *>{&G.front()};
+    auto NewHash = ComputeHash(NodeToDominators);
+    auto OldHash = NewHash;
+    do {
+      OldHash = NewHash;
+      ComputeDominatorsIteration(G, NodeToDominators);
+      NewHash = ComputeHash(NodeToDominators);
+    } while (NewHash != OldHash);
+    return NodeToDominators;
+  }
 
-inline std::vector<std::pair<const Node *, const Node *>>
-ComputeIDom(const GraphTy &G) {
-  auto NodeToDoms = ComputeDominators(G);
-  std::vector<std::pair<const Node *, const Node *>> Res;
-  for (auto &&[NodePtr, Doms] : NodeToDoms)
-    Res.emplace_back(NodePtr, *std::prev(Doms.end(), 2));
-  return Res;
-}
+  inline const Node *Intersect(
+      const Node *First, const Node *Second, auto PO,
+      std::unordered_map<const Node *, const Node *> &IDoms) {
+    auto *Finger1 = First;
+    auto *Finger2 = Second;
+    while (Finger1 != Finger2) {
+      auto Idx1 =
+          std::distance(PO.begin(), std::find(PO.begin(), PO.end(), First));
+      auto Idx2 =
+          std::distance(PO.begin(), std::find(PO.begin(), PO.end(), Second));
+      while (Idx1 < Idx2) {
+        Finger1 = IDoms[Finger1];
+        Idx1 =
+            std::distance(PO.begin(), std::find(PO.begin(), PO.end(), First));
+      }
+      while (Idx2 < Idx1) {
+        Finger2 = IDoms[Finger2];
+        Idx2 =
+            std::distance(PO.begin(), std::find(PO.begin(), PO.end(), Second));
+      }
+    }
+    return Finger1;
+  }
+
+  inline std::unordered_map<const Node *, const Node *> ComputeIDom(
+      const GraphTy &G) {
+    std::unordered_map<const Node *, const Node *> IDom;
+    for (auto &&Nd : G)
+      IDom[&Nd] = nullptr;
+    IDom[&G.front()] = &G.front();
+    bool Changed = true;
+    while (Changed) {
+      Changed = false;
+      auto RPO = llvm::ReversePostOrderTraversal(&G.front());
+      for (auto *CNode : llvm::drop_begin(RPO)) {
+        const auto *NewIdom = *CNode->Parents.begin();
+        for (auto *Parent : llvm::drop_begin(CNode->Parents)) {
+          if (IDom[Parent]) {
+            auto PO = llvm::drop_begin(llvm::post_order(&G.front()));
+            NewIdom = Intersect(Parent, NewIdom, PO, IDom);
+          }
+        }
+        if (IDom[CNode] != NewIdom) {
+          IDom[CNode] = NewIdom;
+          Changed = true;
+        }
+      }
+    }
+    return IDom;
+  }
 
 } // namespace lqvm
--- include/ReducibleGraph.h
+++ include/ReducibleGraph.h
@@ -6,6 +6,10 @@
 // ----------------------------------------------------------------------------
 #pragma once
 
+<<<<<<< HEAD
+=======
+#include <llvm/ADT/GraphTraits.h>
+    >>>>>>> 32e402f (re-initial commit)
 #include <llvm/ADT/STLExtras.h>
 
 #include <algorithm>
@@ -14,149 +18,162 @@
 #include <random>
 #include <set>
 
-// LQVM - Low Quality Virtual Machine
-namespace lqvm {
+    // LQVM - Low Quality Virtual Machine
+    namespace lqvm {
 
-struct Node final : std::vector<Node *> {
-  std::set<Node *> Parents;
-  using ValueTy = unsigned;
-  ValueTy Val;
-  Node(ValueTy Val) : vector(), Val(Val) {}
+  struct Node final : std::vector<Node *> {
+    std::set<Node *> Parents;
+    using ValueTy = unsigned;
+    ValueTy Val;
+    Node(ValueTy Val) : vector(), Val(Val) {}
 
-  void adoptChild(Node *Child) {
-    assert(std::find(begin(), end(), Child) == end() &&
-           "Attempt to duplicate child.");
-    push_back(Child);
-    Child->addParent(this);
-  }
-
-  void addParent(Node *Parent) { Parents.insert(Parent); }
-
-  void abandonChild(Node *Child) {
-    assert(std::find(begin(), end(), Child) != end() &&
-           "Attempt to remove non-existent child.");
-    llvm::erase_value(*this, Child);
-    Child->removeParent(this);
-  }
-
-  void removeParent(Node *Parent) { Parents.erase(Parent); }
-};
-struct GraphTy : public std::vector<Node> {
-  void dumpDot(std::ostream &OS) const;
-};
-
-class ReducibleGraphBuilder final {
-  GraphTy Graph;
-  unsigned MaxSz;
-  std::mt19937 RandEngine;
-
-  unsigned getUniformRandom(unsigned Lower, unsigned Upper) {
-    std::uniform_int_distribution<decltype(RandEngine)::result_type> Dist(
-        Lower, Upper);
-    return Dist(RandEngine);
-  }
-
-  Node *addNode(Node *Nd) {
-    auto *New = insertNode();
-    auto Size = Nd->size();
-    switch (Size) {
-    case 0: {
-      Nd->adoptChild(New);
-      break;
+    void adoptChild(Node *Child) {
+      assert(std::find(begin(), end(), Child) == end() &&
+             "Attempt to duplicate child.");
+      push_back(Child);
+      Child->addParent(this);
     }
-    case 1: {
-      switch (getUniformRandom(0, 2)) {
-      case 0: {
-        auto *Old = Nd->back();
-        Nd->abandonChild(Old);
-        Nd->adoptChild(New);
-        New->adoptChild(Old);
-        break;
-      }
-      case 1: {
-        New->adoptChild(Nd->back());
-        Nd->adoptChild(New);
-        break;
-      }
-      case 2: {
-        Nd->adoptChild(New);
-        break;
-      }
-      };
-      break;
+
+    void addParent(Node *Parent) { Parents.insert(Parent); }
+
+    void abandonChild(Node *Child) {
+      assert(std::find(begin(), end(), Child) != end() &&
+             "Attempt to remove non-existent child.");
+      llvm::erase_value(*this, Child);
+      Child->removeParent(this);
     }
-    case 2: {
-      switch (getUniformRandom(0, 2)) {
+
+    void removeParent(Node *Parent) { Parents.erase(Parent); }
+  };
+  struct GraphTy : public std::vector<Node> {
+    void dumpDot(std::ostream &OS) const;
+  };
+
+  class ReducibleGraphBuilder final {
+    GraphTy Graph;
+    unsigned MaxSz;
+    std::mt19937 RandEngine;
+
+    unsigned getUniformRandom(unsigned Lower, unsigned Upper) {
+      std::uniform_int_distribution<decltype(RandEngine)::result_type> Dist(
+          Lower, Upper);
+      return Dist(RandEngine);
+    }
+
+    Node *addNode(Node *Nd) {
+      auto *New = insertNode();
+      auto Size = Nd->size();
+      switch (Size) {
       case 0: {
-        llvm::for_each(*Nd, [New](Node *Child) { New->adoptChild(Child); });
-        llvm::for_each(*Nd, [Nd](Node *Child) { Nd->abandonChild(Child); });
         Nd->adoptChild(New);
         break;
       }
       case 1: {
-        auto *Old = Nd->back();
-        Nd->abandonChild(Old);
-        Nd->adoptChild(New);
-        New->adoptChild(Old);
+        switch (getUniformRandom(0, 2)) {
+        case 0: {
+          auto *Old = Nd->back();
+          Nd->abandonChild(Old);
+          Nd->adoptChild(New);
+          New->adoptChild(Old);
+          break;
+        }
+        case 1: {
+          New->adoptChild(Nd->back());
+          Nd->adoptChild(New);
+          break;
+        }
+        case 2: {
+          Nd->adoptChild(New);
+          break;
+        }
+        };
         break;
       }
       case 2: {
+        switch (getUniformRandom(0, 2)) {
+        case 0: {
+          llvm::for_each(*Nd, [New](Node *Child) { New->adoptChild(Child); });
+          llvm::for_each(*Nd, [Nd](Node *Child) { Nd->abandonChild(Child); });
+          Nd->adoptChild(New);
+          break;
+        }
+        case 1: {
+          auto *Old = Nd->back();
+          Nd->abandonChild(Old);
+          Nd->adoptChild(New);
+          New->adoptChild(Old);
+          break;
+        }
+        case 2: {
 
-        llvm::for_each(*Nd, [New](Node *Child) { New->adoptChild(Child); });
-        llvm::for_each(*Nd, [Nd](Node *Child) { Nd->abandonChild(Child); });
-        Nd->adoptChild(New);
-        Nd->adoptChild(New->back());
+          llvm::for_each(*Nd, [New](Node *Child) { New->adoptChild(Child); });
+          llvm::for_each(*Nd, [Nd](Node *Child) { Nd->abandonChild(Child); });
+          Nd->adoptChild(New);
+          Nd->adoptChild(New->back());
+          break;
+        }
+        }
         break;
       }
-      }
-      break;
-    }
-    default:
-      assert(0 && "Node can't have more than 2 successors.");
-    };
-    return New;
-  }
-
-public:
-  ReducibleGraphBuilder(unsigned Sz, unsigned long long Seed)
-      : MaxSz(Sz), RandEngine(Seed) {
-    Graph.reserve(MaxSz);
-  }
-
-  void addSelfLoop(Node *Nd) {
-    if (Nd->size() < 2 && std::find(Nd->begin(), Nd->end(), Nd) == Nd->end())
-      Nd->adoptChild(Nd);
-  }
-  Node *insertNode() {
-    Graph.emplace_back(Graph.size());
-    return &Graph.back();
-  }
-
-  void dumpDot(std::ostream &OS) const;
-
-  void generateImpl() {
-    insertNode();
-    for (int I = 0; I < MaxSz; ++I) {
-      auto N = getUniformRandom(0, Graph.size() - 1);
-      auto Op = getUniformRandom(0, 1);
-      switch (Op) {
-      case 0:
-        addNode(&Graph[N]);
-        break;
-      case 1:
-        addSelfLoop(&Graph[N]);
-        break;
+      default:
+        assert(0 && "Node can't have more than 2 successors.");
       };
+      return New;
     }
-    for (auto &&Nd : Graph) {
-      if (llvm::is_contained(Nd, &Nd))
-        Nd.abandonChild(&Nd);
-    }
-  }
 
-  GraphTy generate() {
-    generateImpl();
-    return std::move(Graph);
-  }
-};
+  public:
+    ReducibleGraphBuilder(unsigned Sz, unsigned long long Seed)
+        : MaxSz(Sz), RandEngine(Seed) {
+      Graph.reserve(MaxSz);
+    }
+
+    void addSelfLoop(Node *Nd) {
+      if (Nd->size() < 2 && std::find(Nd->begin(), Nd->end(), Nd) == Nd->end())
+        Nd->adoptChild(Nd);
+    }
+    Node *insertNode() {
+      Graph.emplace_back(Graph.size());
+      return &Graph.back();
+    }
+
+    void dumpDot(std::ostream &OS) const;
+
+    void generateImpl() {
+      insertNode();
+      for (int I = 0; I < MaxSz; ++I) {
+        auto N = getUniformRandom(0, Graph.size() - 1);
+        auto Op = getUniformRandom(0, 1);
+        switch (Op) {
+        case 0:
+          addNode(&Graph[N]);
+          break;
+        case 1:
+          addSelfLoop(&Graph[N]);
+          break;
+        };
+      }
+      for (auto &&Nd : Graph) {
+        if (llvm::is_contained(Nd, &Nd))
+          Nd.abandonChild(&Nd);
+      }
+    }
+
+    GraphTy generate() {
+      generateImpl();
+      return std::move(Graph);
+    }
+  };
+
 } // namespace lqvm
+
+namespace llvm {
+
+template <> class GraphTraits<const lqvm::Node *> {
+public:
+  using NodeRef = const lqvm::Node *;
+  using ChildIteratorType = lqvm::Node::const_iterator;
+  static NodeRef getEntryNode(const lqvm::Node *N) { return N; }
+  static ChildIteratorType child_begin(NodeRef N) { return N->cbegin(); }
+  static ChildIteratorType child_end(NodeRef N) { return N->cend(); }
+};
+} // namespace llvm
